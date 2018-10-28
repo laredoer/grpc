4ceab9fa480ddf81710adfe6f38726c52458f8f4
wule61 <wule61@live.com>
2018-10-28 15:59:47 +0800

    add 拦截器

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"runtime/debug"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	pb "github.com/wule61/grpc/proto"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 
 	"golang.org/x/net/context"
 
@@ -34,8 +39,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	opts := []grpc.ServerOption{
+		grpc.Creds(c),
+		grpc_middleware.WithUnaryServerChain(
+			loggingInterceptor,
+			RecoveryInterceptor,
+		),
+	}
+
 	fmt.Println("grpc server listening at: 50051 port")
-	server := grpc.NewServer(grpc.Creds(c))
+	server := grpc.NewServer(opts...)
 	pb.RegisterSearchServiceServer(server, &SearchService{})
 	server.Serve(conn)
+}
+
+func loggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Printf("gRPC method: %s, %v", info.FullMethod, req)
+	resp, err := handler(ctx, req)
+	log.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	return resp, err
+}
+
+func RecoveryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+			err = status.Errorf(codes.Internal, "Panic err: %v", e)
+		}
+	}()
+	return handler(ctx, req)
 }
